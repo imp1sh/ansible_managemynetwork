eba0b94772220dcce0c57108fa3c5f3cdf9aab94
Jochen Demmer <jochen@winteltosh.de>
2021-11-03 12:46:17 +0100

    not need this file any more

--- roles/ansible_dehydrated/templates/freebsd_miniohook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env/sh
-cp /usr/local/etc/dehydrated/certs/backupofden.libcom.de/fullchain.pem /usr/local/etc/dehydrated/certs/backupofden.libcom.de/public.crt
-cp /usr/local/etc/dehydrated/certs/backupofden.libcom.de/privkey.pem /usr/local/etc/dehydrated/certs/backupofden.libcom.de/private.key
-chown minio:minio /usr/local/etc/dehydrated/certs/backupofden.libcom.de/public.crt
-chown minio:minio /usr/local/etc/dehydrated/certs/backupofden.libcom.de/private.key
